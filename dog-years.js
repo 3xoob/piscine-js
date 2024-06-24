79495d749e1ba85e54ab280992dd9eaf9680205e
aabdulhu <ali.almoumnin@gmail.com>
2024-06-25 02:56:04 +0300

    10

---
+++ dog-years.js
@@ -0,0 +1,15 @@
+const planets = {
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+}
+function dogYears(planet, dogAge) {
+    dogAge = dogAge/31557600;
+    dogAge = dogAge / 7 * planets[planet]
+    return Math.round(dogAge*100) /100 ;
+}
