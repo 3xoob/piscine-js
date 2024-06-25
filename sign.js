7921ee1eb58f345897be7f034b869f3d99ad9c94
aabdulhu <ali.almoumnin@gmail.com>
2024-06-25 03:19:29 +0300

    16

---
+++ sign.js
@@ -0,0 +1,15 @@
+function sign(num) {
+    if (num>0){
+        return 1
+    }else if (num<0){
+        return -1
+    }else {
+        return 0
+    }
+}
+function sameSign(a, b) {
+    if (a === 0 && b === 0) {
+        return true;
+    }
+    return (a > 0 && b > 0) || (a < 0 && b < 0);
+}
