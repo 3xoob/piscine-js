a854e590e02d0ba87312e4b964a768d7d3067836
aabdulhu <ali.almoumnin@gmail.com>
2024-06-26 03:09:28 +0300

    21

---
+++ elementary.js
@@ -0,0 +1,57 @@
+function multiply(a, b) {
+    let result = 0;
+    let isNegative = false;
+    if (a < 0) {
+        a = -a;
+        isNegative = !isNegative;
+    }
+    if (b < 0) {
+        b = -b;
+        isNegative = !isNegative;
+    }
+    for (let i = 0; i < b; i++) {
+        result += a;
+    }
+    if (isNegative) {
+        result = -result;
+    }
+    return result;
+}
+function divide(a, b) {
+    let result = 0;
+    let isNegative = false;
+    if (a < 0) {
+        a = -a;
+        isNegative = !isNegative;
+    }
+    if (b < 0) {
+        b = -b;
+        isNegative = !isNegative;
+    }
+    while (a >= b) {
+        a -= b;
+        result++;
+    }
+    if (isNegative) {
+        result = -result;
+    }
+    return result;
+}
+function modulo(a, b) {
+    let isNegative = false;
+    if (a < 0) {
+        a = -a;
+        isNegative = !isNegative;
+    }
+    if (b < 0) {
+        b = -b;
+        isNegative = !isNegative;
+    }
+    while (a >= b) {
+        a -= b;
+    }
+    if (isNegative) {
+        a = -a;
+    }
+    return a;
+}
