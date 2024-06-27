71eff18b85ff122363735b1d3c9dab09e4d062a4
aabdulhu <ali.almoumnin@gmail.com>
2024-06-27 05:29:23 +0300

    Loop

--- molecules-cells.js
+++ molecules-cells.js
@@ -1,41 +1,26 @@
-function DNAtoRNA(DNA) {
-    let RNA = '';
-    for (let nucleotide of DNA) {
-      switch (nucleotide) {
-        case 'G':
-          RNA += 'C';
-          break;
-        case 'C':
-          RNA += 'G';
-          break;
-        case 'T':
-          RNA += 'A';
-          break;
-        case 'A':
-          RNA += 'U';
-          break;
-      }
-    }
-    return RNA;
+const DNAtoRNA = {
+    G: "C",
+    C: "G",
+    T: "A",
+    A: "U",
+};
+const RNAtoDNA = {
+    C: "G",
+    G: "C",
+    A: "T",
+    U: "A",
+};
+
+function RNA(dna) {
+    return dna
+        .split("")
+        .map((nucleotide) => DNAtoRNA[nucleotide])
+        .join("");
 }
-  
-function RNAtoDNA(RNA) {
-    let DNA = '';
-    for (let nucleotide of RNA) {
-      switch (nucleotide) {
-        case 'C':
-          DNA += 'G';
-          break;
-        case 'G':
-          DNA += 'C';
-          break;
-        case 'A':
-          DNA += 'T';
-          break;
-        case 'U':
-          DNA += 'A';
-          break;
-      }
-    }
-    return DNA;
+
+function DNA(rna) {
+    return rna
+        .split("")
+        .map((nucleotide) => RNAtoDNA[nucleotide])
+        .join("");
 }
