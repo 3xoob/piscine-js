9fb297cbafd50878b5d205f5b4346f9c078e296e
aabdulhu <ali.almoumnin@gmail.com>
2024-06-28 03:50:37 +0300

    Find

--- same-amount.js
+++ same-amount.js
@@ -1,4 +1,6 @@
-function sameAmount(str, regex1, regex2) {
+function sameAmount(str, reg1, reg2) {
+    const regex1 = new RegExp(reg1,'g')
+    const regex2 = new RegExp(reg2,'g')
     const matches1 = str.match(regex1) || [];
     const matches2 = str.match(regex2) || [];
     return matches1.length === matches2.length;
