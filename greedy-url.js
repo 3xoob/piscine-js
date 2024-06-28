dd6f6352c1a12c1003c36a2367b481d7f7d0d54b
aabdulhu <ali.almoumnin@gmail.com>
2024-06-28 04:04:41 +0300

    Find

--- greedy-url.js
+++ greedy-url.js
@@ -1,16 +1,13 @@
-const URL_REGEX = /https?:\/\/[^\s]+/g;
-const GREEDY_QUERY_REGEX = /https?:\/\/[^\s?]+(\?([^=&]+=[^=&]*&){3,}[^=&]+=[^=&]*)/g;
-const NOT_SO_GREEDY_REGEX = /https?:\/\/[^\s?]+(\?([^=&]+=[^=&]*&){1,2}[^=&]+=[^=&]*)/g;
+const urlExp = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?(?:[-a-zA-Z0-9()\[\],@:%_\+.~#?&\/=]*)/g;
 
 function getURL(dataSet) {
-  const urls = dataSet.match(URL_REGEX) || [];
-  return urls;
+  return dataSet.match(urlExp) || [];
 }
 function greedyQuery(dataSet) {
-  const urls = dataSet.match(GREEDY_QUERY_REGEX) || [];
-  return urls;
+  return getURL(dataSet).filter(url => 
+    /\?([^=&]+=[^=&]*&){2,}[^=&]+=[^=&]*/.test(url)
+  );
 }
 function notSoGreedy(dataSet) {
-  const urls = dataSet.match(NOT_SO_GREEDY_REGEX) || [];
-  return urls;
+  return getURL(dataSet).filter(url => /\?([^=&]+=[^=&]*&){1,2}[^=&]+=[^=&]*$/.test(url));
 }
