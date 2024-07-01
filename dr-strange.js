8f656b1bc20f51f5f994cf943c2627b963f8152d
aabdulhu <ali.almoumnin@gmail.com>
2024-07-01 03:39:25 +0300

    Time

---
+++ dr-strange.js
@@ -0,0 +1,36 @@
+function addWeek(date) {
+    const epoch = new Date('0001-01-01');
+    const dayMilliseconds = 24 * 60 * 60 * 1000;
+    const daysDifference = Math.floor((date - epoch) / dayMilliseconds);
+    const weekIndex = daysDifference % 14;
+
+    const daysOfWeek = [
+        'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
+        'secondMonday', 'secondTuesday', 'secondWednesday', 'secondThursday', 'secondFriday', 'secondSaturday', 'secondSunday'
+    ];
+
+    return daysOfWeek[weekIndex];
+}
+
+function timeTravel({ date, hour, minute, second }) {
+    const newDate = new Date(date);
+
+    if (typeof hour === 'number') newDate.setHours(hour);
+    if (typeof minute === 'number') newDate.setMinutes(minute);
+    if (typeof second === 'number') newDate.setSeconds(second);
+
+    return newDate;
+}
+
+console.log(addWeek(new Date('0001-01-01')));
+console.log(addWeek(new Date('0001-01-02')));
+console.log(addWeek(new Date('0001-01-07')));
+console.log(addWeek(new Date('0001-01-08')));
+console.log(addWeek(new Date('0001-01-09')));
+
+console.log(timeTravel({
+    date: new Date('2020-05-29 23:25:22'),
+    hour: 21,
+    minute: 22,
+    second: 22,
+}).toString());
