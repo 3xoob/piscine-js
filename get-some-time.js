606cee2d13e63f1050bf6b0c87f074e7deac7607
aabdulhu <ali.almoumnin@gmail.com>
2024-07-01 04:38:03 +0300

    Time

--- get-some-time.js
+++ get-some-time.js
@@ -1,16 +1,38 @@
 function firstDayWeek(week, year) {
-    let time = new Date(year, 0, 1);
-    let dayOfYear = (week - 1) * 7;
-    time.setDate(time.getDate() + dayOfYear);
-    while (time.getDay() !== 1) {
-        time.setDate(time.getDate() - 1);
+    let time = new Date(year)
+    if (week === 1) {
+        time.setHours(24)
+        return formattedDate(time)
+    }
+    let dayPlus = week * 7 * 24
+    time.setHours(dayPlus - 123)
+
+    function getWeekDay(date) {
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        return days[date.getDay() - 1];
     }
 
     function formattedDate(d) {
-        let month = String(d.getMonth() + 1).padStart(2, '0');
-        let day = String(d.getDate()).padStart(2, '0');
+        let month = String(d.getMonth() + 1);
+        let day = String(d.getDate() - 1);
         let year = String(d.getFullYear());
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+        if (year.length === 1) year = '000' + year;
+        if (year.length === 2) year = '00' + year;
+        if (year.length === 3) year = '0' + year;
+
+
         return `${day}-${month}-${year}`;
     }
-    return formattedDate(time);
+    for (let i = 0; i < 7; i++) {
+        let today = getWeekDay(time)
+        if (today === 'Monday') {
+            let res = formattedDate(time)
+            return res
+        }
+        time.setHours(-24)
+    }
+    return time
 }
