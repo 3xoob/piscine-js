a2844b15f3747fbae633264404deace844cad0ff
aabdulhu <ali.almoumnin@gmail.com>
2024-07-01 03:39:40 +0300

    Time

--- dr-strange.js
+++ dr-strange.js
@@ -21,16 +21,3 @@ function timeTravel({ date, hour, minute, second }) {
 
     return newDate;
 }
-
-console.log(addWeek(new Date('0001-01-01')));
-console.log(addWeek(new Date('0001-01-02')));
-console.log(addWeek(new Date('0001-01-07')));
-console.log(addWeek(new Date('0001-01-08')));
-console.log(addWeek(new Date('0001-01-09')));
-
-console.log(timeTravel({
-    date: new Date('2020-05-29 23:25:22'),
-    hour: 21,
-    minute: 22,
-    second: 22,
-}).toString());
