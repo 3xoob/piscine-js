935d9e68eed22e68482b827141e8289c80ec3572
aabdulhu <ali.almoumnin@gmail.com>
2024-07-19 16:32:17 +0300

    Node

---
+++ verydisco-forever.mjs
@@ -0,0 +1,20 @@
+import { argv } from 'process';
+import { writeFileSync } from 'fs';
+const makeVeryDisco = (word) => {
+    const middle = Math.ceil(word.length / 2);
+    const firstHalf = word.slice(0, middle);
+    const secondHalf = word.slice(middle);
+    return secondHalf + firstHalf;
+};
+const main = () => {
+    const input = argv[2];
+
+    if (!input) {
+        console.log('Please provide a word or sentence as an argument.');
+        return;
+    }
+    const veryDiscoResult = input.split(' ').map(makeVeryDisco).join(' ');
+    writeFileSync('verydisco-forever.txt', veryDiscoResult);
+    console.log('The result has been written to verydisco-forever.txt');
+};
+main();
