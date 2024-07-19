ba589a9f7e9cc9f1466c9e9be7785142383be8f6
aabdulhu <ali.almoumnin@gmail.com>
2024-07-19 17:24:39 +0300

    Node

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -4,39 +4,46 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const dirName = `guests-${randStr()}`
-const dirPath = path.join(ctx.tmpPath, dirName)
-await fs.promises.mkdir(dirPath)
+const randStr = () => Math.random().toString(36).substr(2, 9);
 
-const guestsFile = path.join(dirPath, 'guests.json');
-const vipFile = path.join(dirPath, 'vip.txt');
+export default async function handler(ctx) {
+  const dirName = `guests-${randStr()}`
+  const dirPath = path.join(ctx.tmpPath, dirName)
+  await fs.promises.mkdir(dirPath)
 
-await fs.promises.writeFile(vipFile, '');
+  const guestsFile = path.join(dirPath, 'guests.json');
+  const vipFile = path.join(dirPath, 'vip.txt');
 
-fs.readFile(guestsFile, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+  await fs.promises.writeFile(vipFile, '');
 
-  const guests = JSON.parse(data);
-  const vipGuests = guests.filter((guest) => guest.response === 'YES');
 
-  vipGuests.sort((a, b) => {
-    if (a.lastname < b.lastname) return -1;
-    if (a.lastname > b.lastname) return 1;
-    if (a.firstname < b.firstname) return -1;
-    if (a.firstname > b.firstname) return -1;
-    return 0;
-  });
-
-  const vipList = vipGuests.map((guest, index) => `${index + 1}. ${guest.lastname} ${guest.firstname}`);
-
-  fs.writeFile(vipFile, vipList.join('\n'), (err) => {
+  fs.readFile(guestsFile, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-    } else {
-      console.log('VIP list saved to vip.txt');
+      return;
     }
+
+    const guests = JSON.parse(data);
+    const vipGuests = guests.filter((guest) => guest.response === 'YES');
+
+    vipGuests.sort((a, b) => {
+      if (a.lastname < b.lastname) return -1;
+      if (a.lastname > b.lastname) return 1;
+      if (a.firstname < b.firstname) return -1;
+      if (a.firstname > b.firstname) return -1;
+      return 0;
+    });
+
+    const vipList = vipGuests.map((guest, index) => `${index + 1}. ${guest.lastname} ${guest.firstname}`);
+
+    fs.writeFile(vipFile, vipList.join('\n'), (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('VIP list saved to vip.txt');
+      }
+    });
   });
-});
+
+  return { data: fs.readFileSync(vipFile, 'utf8') };
+}
